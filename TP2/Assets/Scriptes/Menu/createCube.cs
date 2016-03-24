32d6993f005d284e0cc7af55bdda733ae2d58458
bryan <bryanboni74@gmail.com>
2016-03-24 12:28:39 +0100

    ...

---
+++ TP2/Assets/Scriptes/Menu/createCube.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class createCube : MonoBehaviour {
+	public bool createIsClicked;
+
+	// Use this for initialization
+	void Start () {
+		createIsClicked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		GameObject tempCube = GameObject.Find ("cube2");
+
+		if (createIsClicked) {
+			GameObject.Instantiate (tempCube, tempCube.transform.position + Vector3.up, Quaternion.identity);//add 1 unite in the up direction
+		}
+	}
+
+	void OnGUI(){
+		createIsClicked = GUI.Button (new Rect (100, 100, 100, 100), "Create Cube");
+	}
+}
---
+++ TP2/Assets/Scriptes/Menu/menuScript.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuScript : MonoBehaviour {
+
+	public bool buttonIsClicked;
+	// Use this for initialization
+	void Start () {
+		buttonIsClicked = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(buttonIsClicked){
+			UnityEngine.SceneManagement.SceneManager.LoadScene ("AmericanPool");
+		}	
+	}
+
+	void OnGUI(){
+		buttonIsClicked = GUI.Button(new Rect(0,0,100,50), "Start !");
+	}
+}
