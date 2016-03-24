0984f6a48b7371f874adae683b2e3238d2bb1ed1
bryan <bryanboni74@gmail.com>
2016-03-24 09:56:14 +0100

    finish project

---
+++ TP1/Assets/Scripts/camScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camScript : MonoBehaviour {
+	public float speed = 10;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Rotate (-Input.GetAxis("Mouse Y")*speed, Input.GetAxis("Mouse X")*speed, 0.0f);
+		transform.Translate(Input.GetAxis("Horizontal")*speed,0.0f,Input.GetAxis("Vertical")*speed);
+	}
+}
